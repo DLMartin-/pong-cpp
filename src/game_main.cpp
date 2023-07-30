8fa989202628e034c6f6824202734632c82cf97b
Daniel Martin <ashinyacorn@gmail.com>
2023-07-29 23:14:55 -0500

    Updated to include state

--- src/game_main.cpp
+++ src/game_main.cpp
@@ -2,12 +2,20 @@
 
 #include <chrono>
 #include <iostream>
+#include <variant>
 
 #include <SDL2/SDL.h>
 
 namespace {
 using Ticks = std::chrono::duration<double, std::milli>;
 Ticks const MAX_TICKS(1000. / 60.);
+
+struct Pong {
+  unsigned int counter{0};
+  Ticks elapsedTime{0.};
+};
+using ScreenVariant = std::variant<Pong>;
+ScreenVariant screen{};
 
 SDL_Event event{};
 bool isRunning{true};
@@ -16,7 +24,14 @@ unsigned int secondsCounter{0};
 
 } // namespace
 
-[[nodiscard]] GameResult game_main([[maybe_unused]] ArgsVec const &args) noexcept {
+void update(Ticks const &dt);
+void update(Pong &pong, Ticks const &dt);
+
+// void draw();
+// void draw(Pong& pong);
+
+[[nodiscard]] GameResult
+game_main([[maybe_unused]] ArgsVec const &args) noexcept {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     return GameResult::Error_Initialization;
 
@@ -37,25 +52,18 @@ unsigned int secondsCounter{0};
       if (event.type == SDL_QUIT)
         isRunning = false;
     }
+
     auto const newTime = std::chrono::steady_clock::now();
     auto const frameTime =
         std::chrono::duration_cast<Ticks>(newTime - currentTime);
     currentTime = newTime;
-
     accumulator += frameTime;
 
     while (accumulator >= MAX_TICKS) {
       if (!isRunning)
         break;
 
-      frameCounter++;
-      if (frameCounter == 60) {
-        frameCounter = 0;
-        secondsCounter++;
-        if (secondsCounter == 10)
-          isRunning = false;
-        std::cout << "Seconds elapsed: " << secondsCounter << "\n";
-      }
+      update(MAX_TICKS);
       accumulator -= MAX_TICKS;
     }
 
@@ -70,3 +78,18 @@ unsigned int secondsCounter{0};
   return GameResult::Ok;
 }
 
+void update(Ticks const &dt) {
+  std::visit([dt](auto &&screen_) { update(screen_, dt); }, screen);
+}
+
+void update(Pong &pong, Ticks const &dt) {
+  pong.elapsedTime += dt;
+  if (pong.elapsedTime >= Ticks(1000.)) {
+    pong.elapsedTime -= Ticks(1000.);
+    pong.counter += 1;
+
+    std::cout << "(NEW) Seconds elapsed: " << pong.counter << "\n";
+    if (pong.counter == 10)
+      isRunning = false;
+  }
+}
